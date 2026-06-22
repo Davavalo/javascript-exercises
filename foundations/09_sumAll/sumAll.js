1551e0b9d98f86d796c5d8ae5327b49104c7c3c5
Victor <86305205+Davavalo@users.noreply.github.com>
2026-06-22 18:36:05 -0500

    9-10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (startingValue, endingValue) {
+  const arrayOfValues = [];
 
+  if (startingValue < 0 || endingValue < 0) {
+    return "ERROR";
+  }
+
+  if (startingValue % 1 !== 0 || endingValue % 1 !== 0) {
+    return "ERROR";
+  }
+
+  if (typeof startingValue === "string" || typeof endingValue === "string") {
+    return "ERROR";
+  }
+
+  if (startingValue > endingValue) {
+    for (let i = endingValue; i < startingValue + 1; i++) {
+      arrayOfValues.push(i);
+    }
+  } else {
+    for (let i = startingValue; i < endingValue + 1; i++) {
+      arrayOfValues.push(i);
+    }
+  }
+
+  return arrayOfValues.reduce((total, value) => total + value);
 };
 
 // Do not edit below this line
